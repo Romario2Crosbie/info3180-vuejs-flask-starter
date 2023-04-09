41b1e6838635deaa3dcdeabbc0cff0add240c91f
Romario2Crosbie <romario.anonymous2000@gmail.com>
2023-04-09 16:43:28 -0500

    Excercise 1 & 2 Completed (I think)

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    SECRET_KEY = os.environ.get('SECRET_KEY', '64a6e242-6f97-4db9-b6f2-6d2c6b29a6db')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
